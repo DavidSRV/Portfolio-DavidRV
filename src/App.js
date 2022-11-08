2aa0e4a4a1a71c443e997aab72c089fed32f2be1
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-08 12:47:10 -0500

    Se termina portfolio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/portfolio" element={<MainPage />} />
+      <Route path="/" element={<MainPage />} />
     </Routes>
     </div>
   );
