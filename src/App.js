7a37b0684191ddfaf78269935f84c34774b4ca89
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-11 15:59:31 -0500

    Cambio CV

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/Portfolio-DavidRV" element={<MainPage />} />
+      <Route path="/" element={<MainPage />} />
     </Routes>
     </div>
   );
